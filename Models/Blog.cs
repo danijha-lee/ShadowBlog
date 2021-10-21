ff093f100e13eddc439e66179146eebe29d51329
danijha-lee <84164688+danijha-lee@users.noreply.github.com>
2021-10-21 15:36:59 -0400

    updated dbContext. Added blog and BlogPost model

---
+++ Models/Blog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShadowBlog.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Created { get; set; }
+
+        //Navigational property to reference all of my children
+        public ICollection<BlogPost> BlogPosts = new HashSet<BlogPost>();
+    }
+}
---
+++ Models/BlogPost.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShadowBlog.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public int BlogId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Abstract { get; set; }
+        public string Content { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime? Updated { get; set; }
+
+        //Navigational properties
+        public virtual Blog Blog { get; set; }
+    }
+}
